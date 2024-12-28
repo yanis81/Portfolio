03e5a2b845df993eeaa624de485c22bb25c6ee3d
Yanis Lartigue <lartigueyanis@yahoo.com>
2024-12-28 20:16:11 +0100

    convertisseur vers webp

--- app/components/TimelineParcours.tsx
+++ app/components/TimelineParcours.tsx
@@ -21,7 +21,7 @@ export function TimelineParcour() {
               className="rounded-lg h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src={'/imageTimeline/3IL.webp'}
+              src={'/imageTimeline/3IL1.webp'}
               alt="image du logo de l'ESN"
               width={500}
               height={500}
